ec8d3f927c53a30faeb35714f3ef9050ff4433a5
Colors-detective <meissasow99@gmail.com>
2025-12-15 13:12:44 +0900

    Update code with new changes

--- color_experiment.py
+++ color_experiment.py
@@ -223,8 +223,8 @@ if st.session_state.participant_info_done:
 ### Instructions  
 Please look at the colored squqre appearing on your screen and provide a description or a name to it.
 You can provide your responses in two ways: 
--either typed at **Type your response here ⌨️📝** below the image.
-- or through a recorded in  **Record your voice here 🔊🎤**.
+-##either typed at **Type your response here ⌨️📝** below the image.
+-##or through a recorded in  **Record your voice here 🔊🎤**.
  We strongly encourage you to maintain consistency through all the trials using only one input of response.
 For instance, after choosing to type your answer or record it at **Trial 1**, it is expected that you apply the same choice of response input for all the following trials.".  
 Click on **Next⏭️** to move to the following stimulus.  
